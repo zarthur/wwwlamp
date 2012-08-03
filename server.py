403066a5a81a6e2a6ed9b38a8565fe9abb42eaaa
Arthur Neuman <arthur.neuman@gmail.com>
2012-08-02 20:34:39 -0400

    added javascript to allow posting of from data when slider/select boxes changes

--- server.py
+++ server.py
@@ -9,6 +9,16 @@ from mako.lookup import TemplateLookup
 
 lookup = TemplateLookup(directories=['templates'])
 
+def import_rasperrypi():
+    import raspberrypi as hardware
+    available_pins = [7, 8]
+    return available_pins
+
+def import_arduino():
+    import arduino as hardware
+    available_pins = [('0', '1'), ('o', 'c')] 
+    return available_pins
+
 def render(*args, **kwargs):
     """shortcut render function for use with mako"""
     page = args[0]
@@ -19,49 +29,48 @@ class Control(object):
     """Class defining structure of website and providing
     methods to control relays via a serial port
     """
-    def __init__(self, port):
-        try:
-            self.ser = serial.Serial(port, 9600) if port != 'DEBUG' \
-                        else io.StringIO('')
-        except:
-            print("Invalid device")
-            sys.exit()
-
-        self._debug = True if port == 'DEBUG' else False
+    def __init__(self, available_pins):
+        self._debug = True if available_pins == 'DEBUG' else False
+        if not self._debug:
+            self._pin_dealer = hardware.PinDealer(available_pins)
+            self._bedroom_pin = self._pin_dealer.get_pin()
+            self._living_pin = self._pin_dealer.get_pin()
         
         self.board_states = {'bedroom': False, 'living': False}
         self.web_states = {'bedroom': False, 'living': False}
 
     @cherrypy.expose
-    def index(self):
-        """index.html page for site"""
+    def index(self, *args, **kwargs):
+        if kwargs:
+            switch0 = kwargs.get('toggleswitch0', 'off')
+            switch1 = kwargs.get('toggleswitch1', 'off')
+
+            self.web_states['living'] = True if switch0 == 'on' else False
+            self.web_states['bedroom'] = True if switch1 == 'on' else False
+            
+            if self.board_states['living'] != self.web_states['living']:
+                if not self._debug:
+                    self._living_pin.toggle()
+                else:
+                    print('Switching living')
+
+                self.board_states['living'] = self.web_states['living']
+
+            if self.board_states['bedroom'] != self.web_states['bedroom']:
+                if not self._debug:
+                    self._bedroom_pin.toggle()
+                else:
+                    print('Switching bedroom')
+
+                self.board_states['bedroom'] = self.web_states['bedroom']
+
         return render('index.html', states=self.web_states)
 
-    @cherrypy.expose
-    def toggle(self, *args, **kwargs):
-        living = kwargs.get('checkbox1', '')
-        bedroom = kwargs.get('checkbox2', '')
-        
-        self.web_states['living'] = True if living.lower() == 'true' else False
-        self.web_states['bedroom'] = True if bedroom.lower() == 'true' else False
-        
-        if self.board_states['living'] != self.web_states['living']:
-            if not self._debug:
-                self.ser.write('0'.encode())
-            self.board_states['living'] = self.web_states['living']
-
-        if self.board_states['bedroom'] != self.web_states['bedroom']:
-            if not self._debug:
-                self.ser.write('1'.encode())
-            self.board_states['bedroom'] = self.web_states['bedroom']
-
-        return render('index.html', states=self.web_states)
-
-def main(port, ip_addr):
+def main(port, ip_addr, ip_port=8080):
     """start the server"""
     current_dir = os.path.dirname(os.path.abspath(__file__))
     cherrypy.config.update({'server.socket_host': ip_addr,
-                            'server.socket_port': 8080})
+                            'server.socket_port': ip_port})
     conf = {'/public': {'tools.staticdir.on': True,
                         'tools.staticdir.dir': os.path.join(current_dir, 'templates/public')}}
     cherrypy.quickstart(Control(port), '/' , config=conf)
